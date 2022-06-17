67f7686d0cabdab90bdf1371c9680df275e3cbda
diwert-ai <diwert08@gmail.com>
2022-06-18 01:40:20 +0300

    добавлена задача 22. Generate Parentheses

---
+++ Problems/Medium/generate-parentheses.py
@@ -0,0 +1,67 @@
+﻿#https://leetcode.com/problems/generate-parentheses/
+#Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+#Time Complexity : O(\dfrac{4^n}{\sqrt{n}}). Each valid sequence has at most n steps during the backtracking procedure.
+#Space Complexity : O(\dfrac{4^n}{\sqrt{n}}), as described above, and using O(n)O(n) space to store the sequence.
+
+class Solution:
+    def __init__(self):
+        self.max_sum=0
+        self.result=[]
+        self.op1 = 0
+        self.op2 = 0
+    
+    #мое решение   
+    def add_variant(self,cur_sum,l):
+        if len(l) == 2*self.max_sum:
+            if cur_sum == 0:
+                self.result.append(l)
+            return
+        
+        #self.op1 += 1
+        if cur_sum < self.max_sum:
+            self.add_variant(cur_sum+1,l+'(')
+        
+        if cur_sum > 0:
+            self.add_variant(cur_sum-1,l+')')
+        
+        return
+        
+            
+    def generateParenthesis(self, n: int) -> list[str]:
+        self.max_sum = n      
+        self.add_variant(0,'')      
+        return self.result
+
+    #более быстрое решение (~ в 4 раза)
+    def generateParenthesis2(self, n: int) -> list[str]:
+        ans = []
+        def backtrack(S = [], left = 0, right = 0):
+            if len(S) == 2 * n:
+                ans.append("".join(S))
+                return
+            #self.op2+=1
+            if left < n:
+                S.append("(")
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(")")
+                backtrack(S, left, right+1)
+                S.pop()
+        backtrack()
+        return ans
+
+
+def test0():
+    print(*Solution().generateParenthesis(4), sep='\n')
+
+def test1():
+    s = Solution()
+    s.generateParenthesis(14)
+    s.generateParenthesis2(14)
+
+    print(s.op1/s.op2)
+
+if __name__ == '__main__':
+    test1()
--- Problems/test.py
+++ Problems/test.py
@@ -1,4 +1,58 @@
+class Solution:
+    def __init__(self):
+        self.max_sum=0
+        self.result=[]
+        self.ops=0
+        
+    def add_variant(self,cur_sum,l):
+        
+        if len(l) == 2*self.max_sum:
+            if cur_sum == 0:
+                self.result.append(l)
+            return
+
+        self.ops+=1
+        if cur_sum < self.max_sum:
+            self.add_variant(cur_sum+1,l+'(')
+        
+        if cur_sum > 0:
+            self.add_variant(cur_sum-1,l+')')
+        
+        return
+        
+            
+    def generateParenthesis(self, n: int) -> list[str]:
+        self.max_sum = n      
+        self.add_variant(0,'')      
+        return self.result
+
+
+def next_step(cur_sum,max_sum, max_len,l):
+
+    if len(l) == max_len:
+        if cur_sum == 0:
+            print(l,len(l),' ok!')      
+        return
+
+    if cur_sum<max_sum:
+        next_step(cur_sum+1,max_sum,max_len,l+'(')
+
+    if cur_sum>0:
+        next_step(cur_sum-1,max_sum,max_len,l+')')
+
+    return
+
+
 if __name__ == '__main__':
-    a=1
-    b=2
-    print(a+b)
+    #next_step(0,2,4,'')
+    l = []
+    n=15
+    for i in range(n):
+        sol = Solution()
+        sol.generateParenthesis(i)
+        l.append(sol.ops)
+
+    for i in range(1,n-1):
+        print(l[i+1]/l[i])
+        
+
