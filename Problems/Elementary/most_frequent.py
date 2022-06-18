edf095e7b2c7b040132f9f733b4fa5112e30b62e
diwert-ai <diwert08@gmail.com>
2022-06-18 11:30:24 +0300

    добавлена задача The Most Frequent из https://py.checkio.org/

---
+++ Problems/Elementary/most_frequent.py
@@ -0,0 +1,34 @@
+﻿#https://py.checkio.org/en/mission/the-most-frequent/
+
+def most_frequent_my(data: list) -> str:
+    """
+    determines the most frequently occurring string in the sequence.
+    """
+    cnt = 0
+    item = ''
+    for i in data:
+        n_cnt = data.count(i)
+        if n_cnt > cnt:
+            cnt = n_cnt
+            item = i
+    return item
+
+def most_frequent(data: list) -> str:
+    """
+    determines the most frequently occurring string in the sequence.
+    """
+   
+    return max(set(data),key=data.count)
+
+
+
+
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    print("Example:")
+    print(most_frequent(["a", "b", "c", "a", "b", "a"]))
+
+    assert most_frequent(["a", "b", "c", "a", "b", "a"]) == "a"
+
+    assert most_frequent(["a", "a", "bi", "bi", "bi"]) == "bi"
+    print("Done")
--- Problems/test.py
+++ Problems/test.py
@@ -1,58 +1,24 @@
-class Solution:
-    def __init__(self):
-        self.max_sum=0
-        self.result=[]
-        self.ops=0
-        
-    def add_variant(self,cur_sum,l):
-        
-        if len(l) == 2*self.max_sum:
-            if cur_sum == 0:
-                self.result.append(l)
-            return
+def test0():
+    l = ['a','b','bb','bb']
+    cnt = 0
+    item = ''
+    for i in l:
+        n_cnt = l.count(i)
+        if n_cnt > cnt:
+            cnt = n_cnt
+            item = i
 
-        self.ops+=1
-        if cur_sum < self.max_sum:
-            self.add_variant(cur_sum+1,l+'(')
-        
-        if cur_sum > 0:
-            self.add_variant(cur_sum-1,l+')')
-        
-        return
-        
-            
-    def generateParenthesis(self, n: int) -> list[str]:
-        self.max_sum = n      
-        self.add_variant(0,'')      
-        return self.result
+    return item
+
+def test1():
+    l = ['a','b','bb','bb']
+
+    return max(set(l), key = l.count)
 
 
-def next_step(cur_sum,max_sum, max_len,l):
-
-    if len(l) == max_len:
-        if cur_sum == 0:
-            print(l,len(l),' ok!')      
-        return
-
-    if cur_sum<max_sum:
-        next_step(cur_sum+1,max_sum,max_len,l+'(')
-
-    if cur_sum>0:
-        next_step(cur_sum-1,max_sum,max_len,l+')')
-
-    return
 
 
 if __name__ == '__main__':
-    #next_step(0,2,4,'')
-    l = []
-    n=15
-    for i in range(n):
-        sol = Solution()
-        sol.generateParenthesis(i)
-        l.append(sol.ops)
-
-    for i in range(1,n-1):
-        print(l[i+1]/l[i])
+    print(test1())
         
 
