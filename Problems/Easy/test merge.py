6e2a78e59cd6b5a0f52664a9bc573c8ddedf53b1
diwert-ai <diwert08@gmail.com>
2022-06-19 22:55:03 +0300

    добавлен не рекурсивный метод печати слияния двух отсортированных списков

--- Problems/Easy/test merge.py
+++ Problems/Easy/test merge.py
@@ -1,26 +1,65 @@
-﻿#рекурсивный вывод слияния двух списков
-
-def merge_print(a,b):
+﻿#рекурсивный вывод слияния двух отсортированных списков
+def merge_print0(a,b):
     if len(a) == 0:
-        print(*b)
-        return
+       for x in b:
+            print(x, end = ' ')
+       print('') 
+       return
 
     if len(b) == 0:
-        print(*a)
+        for x in a:
+            print(x, end = ' ')
+        print('')
         return
 
     if a[0]>b[0]:
         print(b[0],end=' ')
-        merge_print(a,b[1:])
+        merge_print0(a,b[1:])
     else:
         print(a[0],end=' ')
-        merge_print(a[1:],b)
+        merge_print0(a[1:],b)
 
     return
 
+#не рекурсивный вывод слияния двух отсортированных списков
+def merge_print1(a,b):
+
+    if len(a) == 0:
+       for x in b:
+            print(x, end = ' ')
+       print('') 
+       return
+
+    if len(b) == 0:
+        for x in a:
+            print(x, end = ' ')
+        print('')
+        return
+
+    i_a = i_b = 0
+
+    while i_a < len(a) and i_b < len(b):
+        if a[i_a] < b[i_b]:
+            print(a[i_a], end = ' ')
+            i_a+=1
+        else:
+            print(b[i_b], end = ' ')
+            i_b+=1
+
+    if i_a == len(a):
+        for x in b[i_b:]:
+            print(x, end = ' ')
+    else:
+        for x in a[i_a:]:
+            print(x, end = ' ')
+    print('')
+
+
+
 
 if __name__ == '__main__' :
-    a=[1,2,3,5,6,7,8,8,8,10,100,400]
-    b=[-6,-5,4,5,5,5,5,5,5,6,6,6,6,7,700,800]
+    a=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+    b=[5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-    merge_print(a,b)
+    merge_print0(a,b)
+    merge_print1(a,b)
