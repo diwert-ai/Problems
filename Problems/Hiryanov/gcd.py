0eeed37e9c80eae4a15feaf84c012730c3944eda
diwert-ai <diwert08@gmail.com>
2022-07-01 14:54:06 +0300

    небольшие правки

--- Problems/Hiryanov/gcd.py
+++ Problems/Hiryanov/gcd.py
@@ -4,14 +4,20 @@
 
 import math
 
+#a,b > 0
+def gcd_p(a,b):
+    return a if b == 0 else gcd_p(b,a%b)
+
+#a,b любого знака
 def gcd(a,b):
-    assert a > 0
-    return a if b == 0 else gcd(b,a%b)
+    a = abs(a)
+    b = abs(b)
+    return gcd_p(b,a%b)
 
 
 
 def test():
-    tests = [(1,1),(16,8),(74,2),(73,7),(34,17),(1024,256),(999,16),(5,12)]
+    tests = [(1,1),(-16,-8),(74,2),(-73,7),(34,17),(1024,256),(999,16),(5,12), (12,-6), (-100, 200),(-200,-13)]
 
     for a,b in tests:
         print(f'gcd({a},{b})={gcd(a,b)} math.gcd={math.gcd(a,b)}')
