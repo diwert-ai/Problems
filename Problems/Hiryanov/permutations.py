0cc64018b8cff204eb69b6cd5be6c08f1d1865f0
diwert-ai <diwert08@gmail.com>
2022-07-01 17:35:39 +0300

    небольшая модификация

--- Problems/Hiryanov/permutations.py
+++ Problems/Hiryanov/permutations.py
@@ -2,30 +2,29 @@
 #из лекции Хирьянова https://www.youtube.com/watch?v=2XFaK3bgT7w&list=PLRDzFCPr95fK7tr47883DFUbm4GeOjjc0&index=8
 
 
-def gen_numbers(n:int,m:int,prefix=[]):
+def gen_numbers(n:int,m:int,prefix=''):
     '''функция генерируящая все N ричные числа занимающие m позиций  от 00...0 до N-1N-1...N-1'''
     if m == 0:
-        print(*prefix,sep='')
+        print(prefix,sep='')
         return
 
     for c in range(n):
-        prefix.append(c)
-        gen_numbers(n,m-1,prefix)
-        prefix.pop()
+        gen_numbers(n,m-1,prefix+str(c))
 
-def gen_permutations(n:int,m:int,prefix=[]):
+
+def gen_permutations(n:int,m:int,prefix=''):
     '''функция генерируящая все перестановки длины n - модификация gen_numbers'''
     if m == 0:
-        print(*prefix,sep='')
+        print(prefix,sep='')
         return
 
-    for c in [c for c in range(n) if c not in prefix]:
-        prefix.append(c)
-        gen_permutations(n,m-1,prefix)
-        prefix.pop()
+    for c in [c for c in range(n) if str(c) not in prefix]:
+        gen_permutations(n,m-1,prefix+str(c))
+
 
 if __name__ == '__main__':
     n = 4
+    #gen_numbers(2,2)
     gen_permutations(n,n)
 
 
