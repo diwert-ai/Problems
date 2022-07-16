62902a7c6c948b46336c2fe75bddc5c8aa0696ce
diwert-ai <diwert08@gmail.com>
2022-07-16 22:06:13 +0300

    just test

--- Problems/test.py
+++ Problems/test.py
@@ -1,5 +1,17 @@
-a = [0,4,6,8,10,0]
+n = 5
+a = [[i+n*j+1 for i in range(n)] for j in range(n)]
+s = "{:2d} "*n
+for r in a:
+    print(s.format(*r))
+print('')
 
-print(all([i%2==0 for i in a]))
+d0 = [[row[i+j] for i,row in enumerate(a) if i+j < len(row)] for j in range(len(a)-3)]
+d1 = [[row[i-j] for i,row in enumerate(a) if i-j > -1] for j in range(1,len(a)-3)]
+
+d2 = [[row[-i+j-1] for i,row in enumerate(a) if -i+j-1 < 0] for j in range(len(a)-3)]
+d3 = [[row[-i-j-1] for i,row in enumerate(a) if -i-j-1 > -len(row)-1] for j in range(1,len(a)-3)]
+
+print(*(d0+d1+d2+d3),sep='\n')
+
 
 
