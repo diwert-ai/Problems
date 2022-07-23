da50ecf2d7cf575e9b3330417e350331504a10a6
diwert-ai <diwert08@gmail.com>
2022-07-23 21:54:57 +0300

    can pass checkio

--- Problems/Checkio/Moderate/can pass.py
+++ Problems/Checkio/Moderate/can pass.py
@@ -1,4 +1,13 @@
-﻿from collections import deque
+﻿# https://py.checkio.org/ru/mission/can-pass/
+# Дана матрица с цифрами и координаты (строка и столбец) двух клеток
+# с одинаковым значением. Вы можете перемещаться между соседними клетками
+# с одинаковыми значениями по вертикали и горизонтали. Определите есть ли
+# путь между двумя заданными клетками.
+# Матрица представлена, как массив массивов с цифрами. Координаты представлены,
+# как два числа: строка и столбец. Результат проверяется, как булево значение.
+# Есть путь существует - True, иначе - False.
+
+from collections import deque
 
 
 def bfs_p(start_vertex, G, parents):
@@ -31,7 +40,6 @@ def can_pass(matrix, first, second):
     value = matrix[first[0]][first[1]]
     if value != matrix[second[0]][second[1]]:
         return False
-
     n = len(matrix)
     m = len(matrix[0])
     G = dict()
@@ -45,6 +53,25 @@ def can_pass(matrix, first, second):
                         s.add((l, k))
                 G[(i, j)] = s
     return find_path(first, second, G) is not None
+
+from itertools import chain, product, starmap
+
+
+# best clear solution
+# ref: https://py.checkio.org/mission/can-pass/publications/gyahun_dash/python-3/itertools/
+def can_pass_clear(matrix, first, second):
+    digit = matrix[first[0]][first[1]]
+    cells = product(range(len(matrix)), range(len(matrix[0])))
+    living = {(y, x) for y, x in cells if matrix[y][x] == digit}
+
+    neighbors = lambda y, x: ((y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1))
+    tips = {first}
+    while tips:
+        tips = set(chain.from_iterable(starmap(neighbors, tips))) & living
+        living -= tips
+        if second in tips: return True
+
+    return False
 
 
 if __name__ == '__main__':
@@ -64,3 +91,5 @@ if __name__ == '__main__':
                      (0, 0, 0, 0, 0, 2),
                      (2, 2, 2, 2, 2, 2),),
                      (3, 3), (6, 0)) == False, 'First example'
+    print('All done!')
+
