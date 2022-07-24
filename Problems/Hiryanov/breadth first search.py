525401f5f033adbca50c71ce2b96847f34c3ede3
diwert-ai <diwert08@gmail.com>
2022-07-24 03:46:36 +0300

    bfs

--- Problems/Hiryanov/breadth first search.py
+++ Problems/Hiryanov/breadth first search.py
@@ -30,8 +30,8 @@ Graphs = [{0: {1, 11, 12},
            9: {11},
            10: {6, 0},
            11: {0, 12, 14, 3, 9},
-           12: {0, 11, 8},
-           13: {7, 5},
+           12: {0, 11, 8, 13},
+           13: {7, 5, 12},
            14: {11}},
           {0: {18, 1, 14},
            1: {0, 14, 15},
@@ -84,16 +84,31 @@ Graphs = [{0: {1, 11, 12},
            'L': {'J', 'K'}},
           {'a': {'b'},
            'b': {'c', 'e', 'f'},
-           'c': {'d', 'g'},
+           'c': {'g'},
            'd': {'c', 'h'},
            'e': {'a', 'f'},
-           'f': {'g'},
+           'f': {},
            'g': {'f'},
-           'h': {'d', 'g'}},
+           'h': {'g'}},
           {0: {1},
            1: {2, 3},
            2: {0},
-           3: {}}]
+           3: {}},
+          {'a': {'b', 'c', 'd'},
+           'b': {'a', 'e'},
+           'c': {'a', 'e'},
+           'd': {'a', 'e'},
+           'e': {'b', 'c', 'd', 'f', 'g', 'h'},
+           'f': {'e', 'j'},
+           'g': {'e', 'j'},
+           'h': {'e', 'j'},
+           'j': {'f', 'g', 'h'}},
+          {1: {2},
+           2: {3, 5},
+           3: {4},
+           4: {},
+           5: {6},
+           6: {4}}]
 
 
 # функция вычисляющая расстояния от start_vertex
@@ -222,11 +237,148 @@ def test3():
     start = 'a8'
     end = 'h1'
     print(f'from {start} to {end} knight path is: {find_path(start, end, G)}')
-    pass
+    print('')
 
+
+# нахождение кратчайшего цикла
+# в орграфе (для обычного графа
+# будет возвращать элементарный
+# цикл длины 2 - т.е. ребро)
+# ref: http://e-maxx.ru/algo/bfs
+# производим поиск в ширину из каждой вершины;
+# как только в процессе обхода мы пытаемся
+# пойти из текущей вершины по какому-то ребру
+# в уже посещённую вершину, то это означает,
+# что мы нашли кратчайший цикл, и останавливаем
+# обход в ширину; среди всех таких найденных циклов
+# (по одному от каждого запуска обхода) выбираем кратчайший.
+def bfs_cycle(vertex, G, used):
+    used.add(vertex)
+    queue = deque([vertex])
+    while queue:
+        cur_v = queue.popleft()
+        for neighbor in G[cur_v]:
+            if neighbor not in used:
+                used.add(neighbor)
+                queue.append(neighbor)
+            else:
+                return neighbor, cur_v
+    return (None, None)
+
+
+def shortest_cycle(G):
+    shortest_cycle = None
+    for vertex in G:
+        used = set()
+        cycle_start, cycle_end = bfs_cycle(vertex, G, used)
+        if cycle_start is not None:
+            cycle_path = find_path(cycle_start, cycle_end, G)
+            if cycle_path is not None:
+                if (shortest_cycle is None or
+                        len(cycle_path) < len(shortest_cycle)):
+                    shortest_cycle = cycle_path
+    return shortest_cycle
+
+
+def test4():
+    for i, G in enumerate(Graphs):
+        print(f'shortest cycle of graph #{i} is: {shortest_cycle(G)}')
+    print('')
+
+
+# ref: http://e-maxx.ru/algo/bfs
+# нахождение всех вершин и всех ребер
+# на каком-либо кратчайшем пути из a в b.
+# Для этого надо запустить 2 поиска в ширину:
+# из a, и из b. Обозначим через d_a[] массив
+# кратчайших расстояний, полученный в результате
+# первого обхода, а через d_b[] — в результате
+# второго обхода. Теперь для любой вершины v
+# легко проверить, лежит ли он на каком-либо
+# кратчайшем пути: критерием будет условие
+# d_a[v] + d_b[v] = d_a[b].
+def all_verts(a, b, G):
+    all_v = []
+    d_a = dict()
+    d_b = dict()
+    bfs_d(a, G, d_a)
+    bfs_d(b, G, d_b)
+    for v in G:
+        if v in d_a and v in d_b and d_a[v] + d_b[v] == d_a[b]:
+            all_v.append(v)
+    return all_v
+
+
+# ref: http://e-maxx.ru/algo/bfs
+# Найти все рёбра, лежащие на каком-либо кратчайшем
+# пути между заданной парой вершин (a,b). Для этого
+# надо запустить 2 поиска в ширину: из a, и из b.
+# Обозначим через d_a[] массив кратчайших расстояний,
+# полученный в результате первого обхода, а через d_b[]
+# — в результате второго обхода. Теперь для любого ребра
+# (u,v) легко проверить, лежит ли он на каком-либо
+# кратчайшем пути: критерием будет условие
+# d_a[u] + 1 + d_b[v] = d_a[b].
+def all_edges(a, b, G):
+    all_e = []
+    d_a = dict()
+    d_b = dict()
+    bfs_d(a, G, d_a)
+    bfs_d(b, G, d_b)
+    for u in G:
+        for v in G[u]:
+            if (u in d_a and u in d_b and v in d_a and v in d_b and
+                    d_a[u] + 1 + d_b[v] == d_a[b]):
+                all_e.append((u, v))
+    return all_e
+
+
+# ref: http://e-maxx.ru/algo/bfs
+# Найти кратчайший чётный путь в графе (т.е. путь чётной длины).
+# Для этого надо построить вспомогательный граф, вершинами которого
+# будут состояния (v,c), где v — номер текущей вершины,
+# c = 0...1 — текущая чётность. Любое ребро (a,b) исходного графа
+# в этом новом графе превратится в два ребра ((u,0),(v,1)) и ((u,1),(v,0)
+# После этого на этом графе надо обходом в ширину найти кратчайший путь
+# из стартовой вершины в конечную, с чётностью, равной 0.
+def find_even_path(start, end, G):
+    H = dict()
+    for g in G:
+        H[(g, 0)] = set()
+        H[(g, 1)] = set()
+
+    for u in G:
+        for v in G[u]:
+            H[(u, 0)].add((v, 1))
+            H[(u, 1)].add((v, 0))
+    return find_path((start, 0), (end, 0), H)
+
+
+def test5():
+    start = 'a'
+    end = 'j'
+    for i, G in enumerate(Graphs):
+        if start in G and end in G:
+            print(f'all vertexes between {start} and {end} in graph #{i}:' +
+                  f'{all_verts(start, end, G)}')
+            print(f'all edges between {start} and {end} in graph #{i}:' +
+                  f'{all_edges(start, end, G)}')
+    print('')
+
+
+def test6():
+    start = 1
+    end = 4
+    for i, G in enumerate(Graphs):
+        if start in G and end in G:
+            print(f'[graph #{i}] from {start} to {end} even path: ' +
+                  f'{find_even_path(start, end, G)}')
 
 if __name__ == '__main__':
     test0()
     test1()
     test2()
     test3()
+    test4()
+    test5()
+    test6()
