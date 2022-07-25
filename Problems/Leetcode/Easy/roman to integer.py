672415543f9843337dc67564c2ec88f025819dce
diwert-ai <diwert08@gmail.com>
2022-07-26 02:58:27 +0300

    roman to int

---
+++ Problems/Leetcode/Easy/roman to integer.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/roman-to-integer/
+
+
+class Solution:
+    def roman_to_int(self, s: str) -> int:
+        result = 0
+        roman = {'I': 1,
+                 'V': 5,
+                 'X': 10,
+                 'L': 50,
+                 'C': 100,
+                 'D': 500,
+                 'M': 1000}
+        list_s = list(s)
+        n = len(list_s)
+        i = 0
+        for i in range(n - 1):
+            d_i = roman[list_s[i]]
+            if d_i >= roman[list_s[i + 1]]:
+                result += d_i
+            else:
+                result -= d_i
+        result += roman[list_s[n - 1]]
+        return result
+
+
+if __name__ == '__main__':
+    tests = [("III", 3),
+             ("LVIII", 58),
+             ("MCMXCIV", 1994)]
+    for test_case, (test_s, right_answer) in enumerate(tests):
+        print(f'test case#{test_case}: {Solution().roman_to_int(test_s)} == {right_answer}')
