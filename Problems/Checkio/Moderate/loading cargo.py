c9c5f974c9a0e43501984acfdddea76d1e6836f5
diwert-ai <diwert08@gmail.com>
2022-07-26 01:15:16 +0300

    loading cargo checkio

---
+++ Problems/Checkio/Moderate/loading cargo.py
@@ -0,0 +1,36 @@
+﻿# https://py.checkio.org/ru/mission/loading-cargo/
+# Вам дан список, сколько весят предметы для погрузки.
+# Необходимо распределить их все так чтобы разница между
+# общим весом в каждой руке была минимально возможной.
+# Входные данные: Список весов предметов, как список (list)
+# целых чисел (int).
+# Выходные данные: Минимально возможная разница между весами
+# в каждой руке, как целое число (int).
+
+# вариация на тему дискретной задаче о рюкзаке (knapsack problem)
+
+
+def checkio(data):
+    S = sum(data)
+    M = S//2
+    N = len(data)
+    F = [[0]*(N+1) for i in range(M+1)]
+    for i in range(1, N+1):
+        for k in range(1, M+1):
+            if data[i-1] <= k:
+                F[k][i] = max(F[k][i-1], data[i-1] + F[k-data[i-1]][i-1])
+            else:
+                F[k][i] = F[k][i-1]
+    return abs(S - 2*F[M][N])
+    
+    
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([10, 10]) == 0, "1st example"
+    assert checkio([10]) == 10, "2nd example"
+    assert checkio([5, 8, 13, 27, 14]) == 3, "3rd example"
+    assert checkio([5, 5, 6, 5]) == 1, "4th example"
+    assert checkio([12, 30, 30, 32, 42, 49]) == 9, "5th example"
+    assert checkio([1, 1, 1, 3]) == 0, "6th example"
+    print('All done!')
+
--- Problems/test.py
+++ Problems/test.py
@@ -1,39 +1,19 @@
-﻿count = 0
+﻿data = [1,1,1,3]
 
-def unix_match(filename: str, pattern: str) -> bool:
-    global count 
-    count += 1
-    if not pattern:
-        return not filename
+data.sort()
+all_sum = sum(data)
+print(sum(data))
 
-    first_match = bool(filename) and pattern[0] in [filename[0], '?']
+half_sum = 0
 
-    if pattern[0] == '*':
-        return unix_match(filename, pattern[1:]) or (bool(filename) and unix_match(filename[1:], pattern))
-    else:
-        return first_match and unix_match(filename[1:], pattern[1:])
+for i in range(len(data)):
+    half_sum += data[i]
+    if half_sum > all_sum:
+        break
+    i += 1
 
-
-
-
+print(i)
+print(min(all_sum - half_sum, half_sum - data[i-1]))
 
 
-if __name__ == '__main__':
-    '''
-    print("Example:")
-    print(unix_match('somefile.txt', '*'))
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert unix_match('somefile.txt', '*') == True
-    assert unix_match('other.exe', '*') == True
-    assert unix_match('my.exe', '??.txt') == False
-    assert unix_match('log1.txt', 'log?.txt') == True
-    assert unix_match('log12.txt', 'log?.txt') == False
-    '''
 
-    assert unix_match('teRxt*sdaskfjldsdfnerxtrhdaksjdhgfd', 'te?xt*ne?xt?*?') == True
-    print(count, len('teRxt*sdaskfjldsdfnerxtrhdaksjdhgfd') * len('te?xt*ne?xt?*?'))
-    count = 0
-    assert unix_match_dp('teRxt*sdaskfjldsdfnerxtrhdaksjdhgfd', 'te?xt*ne?xt?*?') == True
-    print(count, len('teRxt*sdaskfjldsdfnerxtrhdaksjdhgfd') * len('te?xt*ne?xt?*?'))
-    assert unix_match('l?l', '??') == False
-    print("Coding complete? Click 'Check' to earn cool rewards!")
