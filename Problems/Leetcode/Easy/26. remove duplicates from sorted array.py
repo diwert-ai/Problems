21506cd91211cb841bb22362fc1d957ab6a52adc
diwert-ai <diwert08@gmail.com>
2022-08-02 15:52:30 +0300

    pep8

--- Problems/Leetcode/Easy/26. remove duplicates from sorted array.py
+++ Problems/Leetcode/Easy/26. remove duplicates from sorted array.py
@@ -1,27 +1,27 @@
-﻿#https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+﻿# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-#Given an integer array nums sorted in non-decreasing order, 
-#remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+# Given an integer array nums sorted in non-decreasing order,  
+# remove the duplicates in-place such that each unique element appears only once.
+# The relative order of the elements should be kept the same.
+# Since it is impossible to change the length of the array in some languages,  
+# you must instead have the result be placed in the first part of the array nums. 
+# More formally,  if there are k elements after removing the duplicates,  
+# then the first k elements of nums should hold the final result. 
+# It does not matter what you leave beyond the first k elements.
+# Return k after placing the final result in the first k slots of nums.
+# Do not allocate extra space for another array.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+from typing import List
 
-#Since it is impossible to change the length of the array in some languages, 
-#you must instead have the result be placed in the first part of the array nums. 
-#More formally, if there are k elements after removing the duplicates, 
-#then the first k elements of nums should hold the final result. 
-#It does not matter what you leave beyond the first k elements.
-
-#Return k after placing the final result in the first k slots of nums.
-
-#Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
-from typing import *
 
 class Solution:
-    #мое решение O(n) - время; O(1)  - память
-    def removeDuplicates(self, nums: List[int]) -> int:
-        
+    # мое решение O(n) - время; O(1)  - память
+    @staticmethod
+    def remove_duplicates(nums: List[int]) -> int:
         i = 1
         j = 0
         while j < len(nums) and i < len(nums):
-            while  nums[j] == nums[i-1]: 
+            while nums[j] == nums[i-1]: 
                 j += 1
                 if j == len(nums):
                     break
@@ -29,30 +29,32 @@ class Solution:
             if j == len(nums):
                 break
 
-            nums[i]=nums[j]
-            i+=1
+            nums[i] = nums[j]
+            i += 1
                       
-        for j in range(i,len(nums)):
-            nums[j] = '_'
+        for _ in range(i, len(nums)):
+            nums.pop()
             
         return i
 
-    #решение https://leetcode.com/problems/remove-duplicates-from-sorted-array/discuss/302016/Python-Solution
-    def removeDuplicates2(self, nums: List[int]) -> int:
-        
+    # решение most voted
+    # https://leetcode.com/problems/remove-duplicates-from-sorted-array/discuss/302016/Python-Solution
+    @staticmethod
+    def remove_duplicates_mv(nums: List[int]) -> int:        
         x = 1
         for i in range(len(nums)-1):
-            if(nums[i]!=nums[i+1]):
+            if nums[i] != nums[i+1]:
                 nums[x] = nums[i+1]
-                x+=1
-        return(x)
+                x += 1
+        return x
 
 
 def test0():
-    m = [1,2,2,2,2,3,3,3,4,7,7,7,8,9,9,9,9,20]
+    m = [1, 2, 2, 2, 2, 3, 3, 3, 4, 7, 7, 7, 8, 9, 9, 9, 9, 20]
 
-    print(Solution().removeDuplicates(m))
+    print(Solution.remove_duplicates(m))
     print(m)
+
 
 if __name__ == '__main__':
     test0()
