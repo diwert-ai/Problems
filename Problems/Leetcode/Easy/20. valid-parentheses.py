2d03cd7b7bb4e3969e0cce503efe6012cc1907fe
diwert-ai <diwert08@gmail.com>
2022-08-02 15:38:38 +0300

    pep8

--- Problems/Leetcode/Easy/20. valid-parentheses.py
+++ Problems/Leetcode/Easy/20. valid-parentheses.py
@@ -1,33 +1,37 @@
-﻿#https://leetcode.com/problems/valid-parentheses/
-#Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-#
-#An input string is valid if:
-#
-#Open brackets must be closed by the same type of brackets.
-#Open brackets must be closed in the correct order.
+﻿# https://leetcode.com/problems/valid-parentheses/
+# Given a string s containing just the characters '(', ')',
+# '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+
+
 class Solution:
-    def isValid(self, s: str) -> bool:
-        st =[]
+    @staticmethod
+    def is_valid(s: str) -> bool:
+        stack = []
         for c in s:
             if c in '({[':
-                st.append(c)
+                stack.append(c)
             else:
                 assert c in ')}]', f"right brace expected: got {c}"
 
-                if len(st) == 0:
+                if len(stack) == 0:
                     return False
 
-                left=st.pop()
-                if  (c == ')' and left != '(') or \
-                    (c == '}' and left != '{') or \
-                    (c == ']' and left != '['): 
+                left = stack.pop()
+                if (c == ')' and left != '(') or \
+                   (c == '}' and left != '{') or \
+                   (c == ']' and left != '['):
                     return False
 
+        return len(stack) == 0
 
-        return len(st) == 0
 
 if __name__ == '__main__':
-    tests = [('{}', True),('{}{}()((((([[[[]]]])))))', True),('{[}]',False), ('({)[({]})}',False)]
-    sol = Solution()
-    for s,r in tests:
-        print(sol.isValid(s),r)
+    tests = [('{}', True),
+             ('{}{}()((((([[[[]]]])))))', True),
+             ('{[}]', False),
+             ('({)[({]})}', False)]
+    for test_s, test_r in tests:
+        print(Solution.is_valid(test_s), test_r)
