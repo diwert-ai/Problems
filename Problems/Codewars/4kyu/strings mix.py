72c46e80f9f53b00c782e487ea08ab581f17598a
diwert-ai <diwert08@gmail.com>
2022-09-04 19:07:50 +0300

    strings mix codewars

--- Problems/Codewars/4kyu/strings mix.py
+++ Problems/Codewars/4kyu/strings mix.py
@@ -1,4 +1,5 @@
-﻿def mix(s1, s2):
+# https://www.codewars.com/kata/5629db57620258aa9d000014
+def mix(s1, s2):
     # noinspection SpellCheckingInspection
     letters = 'abcdefghijklmnopqrstuvwxyz'
     result = []
