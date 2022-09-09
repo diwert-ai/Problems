4382da17e123116ce8a5a18085c3bbba8903db82
diwert-ai <diwert08@gmail.com>
2022-09-09 19:58:58 +0300

    PEP 8

--- Problems/lib/mystack.py
+++ Problems/lib/mystack.py
@@ -1,17 +1,14 @@
-﻿#класс стек для задач из курса Хирьянова
+﻿# класс стек для задач из курса Хирьянова
 
 class MyStack:
-  def __init__(self):
-    self.stack = []
-    return
+    def __init__(self):
+        self.stack = []
 
-  def push(self,a):
-    self.stack.append(a)
-    return
-  
-  def pop(self):
-    return self.stack.pop()
-  
-  def is_empty(self):
-    return len(self.stack)==0
+    def push(self, a):
+        self.stack.append(a)
 
+    def pop(self):
+        return self.stack.pop()
+
+    def is_empty(self):
+        return len(self.stack) == 0
