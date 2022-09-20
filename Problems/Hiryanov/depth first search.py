9a777424a96c79461d639875ef591ab3731e617a
diwert-ai <diwert08@gmail.com>
2022-09-20 17:01:38 +0300

    PEP 8

--- Problems/Hiryanov/depth first search.py
+++ Problems/Hiryanov/depth first search.py
@@ -2,30 +2,30 @@
 # из лекции Хирьянова:
 # https://www.youtube.com/watch?v=sBJ7ana1fgI&list=PLRDzFCPr95fK7tr47883DFUbm4GeOjjc0&index=23
 
-# позволяет:
+# Позволяет:
 # 1. выделить компоненту связности;
 # 2. подсчитать кол-во компонент;
 # 3. найти простой цикл (хотя бы один, если существует);
-# 4. определить граф двудольный или нет (можно ли расскрасить
+# 4. определить граф двудольный или нет (можно ли раскрасить
 # вершины графа в 2 цвета так, что любое ребро соединяет
 # только разноцветные вершины)
 # 5. найти мосты и точки сочленения
-# 6. c орграфами: выделение компонент сильной связности -
+# 6. с орграфами: выделение компонент сильной связности -
 # делается 2 обхода в глубину - алгоритм Косарайю
 # 7. топологическая сортировка
 
 
-def dfs(vertex, G, used=None):
+def dfs(vertex, g, used=None):
     used = used or set()
     print(vertex, end=' ')
     used.add(vertex)
-    for neighbor in G[vertex]:
+    for neighbor in g[vertex]:
         if neighbor not in used:
-            dfs(neighbor, G, used)
+            dfs(neighbor, g, used)
 
 
 def test0():
-    G = {0: {18, 1, 14},
+    g = {0: {18, 1, 14},
          1: {0, 14, 15},
          2: {1, 15, 17, 3},
          3: {2, 7, 4},
@@ -45,34 +45,34 @@ def test0():
          17: {2},
          18: {0}}
     print(f'traversal of the graph in depth: ', end='')
-    dfs(0, G)
+    dfs(0, g)
     print('')
 
 
 # видоизмененная функция обхода для подсчета компонент связности
-def dfs_c(vertex, G, used):
+def dfs_c(vertex, g, used):
     print(vertex, end=' ')
     used.add(vertex)
-    for neighbor in G[vertex]:
+    for neighbor in g[vertex]:
         if neighbor not in used:
-            dfs_c(neighbor, G, used)
+            dfs_c(neighbor, g, used)
 
 
 # процедура подсчета компонент связности
-def number_of_connectivity_components(G):
+def number_of_connectivity_components(g):
     used = set()
-    N = 0
-    for vertex in G:
+    n = 0
+    for vertex in g:
         if vertex not in used:
-            print(f'graph component #{N}: ', end='')
-            dfs_c(vertex, G, used)
+            print(f'graph component #{n}: ', end='')
+            dfs_c(vertex, g, used)
             print('')
-            N += 1
-    return N
+            n += 1
+    return n
 
 
 def test1():
-    G = {0: {18, 1, 14},
+    g = {0: {18, 1, 14},
          1: {0, 14, 15},
          2: {1, 15, 17},
          3: {7},
@@ -91,7 +91,7 @@ def test1():
          16: {14},
          17: {2},
          18: {0}}
-    print(f'N comps = {number_of_connectivity_components(G)}')
+    print(f'n comps = {number_of_connectivity_components(g)}')
     print('')
 
 # Алгоритм Косарайю выделения компонент сильной связности
@@ -103,105 +103,105 @@ def test1():
 
 
 # прямой проход
-def dfs_k(vertex, G, used, stack):
+def dfs_k(vertex, g, used, stack):
     print(vertex, end=' ')
     used.add(vertex)
-    for neighbor in G[vertex]:
+    for neighbor in g[vertex]:
         if neighbor not in used:
-            dfs_k(neighbor, G, used, stack)
+            dfs_k(neighbor, g, used, stack)
     stack.append(vertex)
 
 
 # обратный проход
-def dfs_k_backward(vertex, G, used):
+def dfs_k_backward(vertex, g, used):
     print(vertex, end=' ')
     used.add(vertex)
-    for neighbor in G[vertex]:
+    for neighbor in g[vertex]:
         if neighbor not in used:
-            dfs_k_backward(neighbor, G, used)
+            dfs_k_backward(neighbor, g, used)
 
 
 # процедура инвертирования графа
-def invert(G: dict):
-    H = dict()
-    for vertex in G:
-        H[vertex] = set()
-    for vertex in G:
-        for v in G[vertex]:
-            H[v].add(vertex)
-    return H
+def invert(g: dict):
+    h = dict()
+    for vertex in g:
+        h[vertex] = set()
+    for vertex in g:
+        for v in g[vertex]:
+            h[v].add(vertex)
+    return h
 
 
 # форматированный вывод графа
-def printg(G: dict):
-    for vertex in G:
-        print(f'{vertex}: {G[vertex]}')
+def print_graph(g: dict):
+    for vertex in g:
+        print(f'{vertex}: {g[vertex]}')
 
 
 # процедура подсчета компонент
 # сильной связности орграфа
-def n_strongly_comp(G):
+def n_strongly_comp(g):
     used = set()
     stack = []
-    N = 0
+    n = 0
     # делаем прямой обход орграфа и заполняем попутно стек
     # считаем компоненты слабой связности
-    for vertex in G:
+    for vertex in g:
         if vertex not in used:
-            print(f'graph weekly connected component #{N}: ', end='')
-            dfs_k(vertex, G, used, stack)
+            print(f'graph weekly connected component #{n}: ', end='')
+            dfs_k(vertex, g, used, stack)
             print('')
-            N += 1
-    print(f'N weekly comps = {N}')
+            n += 1
+    print(f'n weekly comps = {n}')
     print(f'stack: {stack}')
     # получаем инвертированный орграф
-    H = invert(G)
+    h = invert(g)
     print(f'inverted graph:')
-    printg(H)
+    print_graph(h)
     # обходим инвертированный орграф
     # учитывая порядок вершин в стеке
     # считаем компоненты сильной связности
-    N = 0
+    n = 0
     used = set()
     print('')
-    while stack != []:
+    while stack:
         vertex = stack.pop()
         if vertex not in used:
-            print(f'strongly comp #{N}: ', end='')
-            dfs_k_backward(vertex, H, used)
+            print(f'strongly comp #{n}: ', end='')
+            dfs_k_backward(vertex, h, used)
             print('')
-            N += 1
-    return N
+            n += 1
+    return n
 
 
 def test2():
-    Graphs = [{'A': {'B'},
+    graphs = [{'A': {'B'},
                'B': {'C', 'D'},
                'C': {'A'},
                'D': {'E'},
                'F': {'D'},
                'E': {'F'},
-               'G': {'F', 'H'},
-               'H': {'I'},
+               'g': {'F', 'h'},
+               'h': {'I'},
                'I': {'J'},
-               'J': {'G'},
+               'J': {'g'},
                'K': {'J'}},
               {'A': {'B'},
                'B': {'C', 'D'},
                'C': {'E'},
                'D': {},
                'E': {'D', 'F'},
-               'F': {'G'},
-               'G': {'E', 'H'},
-               'H': {}},
+               'F': {'g'},
+               'g': {'E', 'h'},
+               'h': {}},
               {'A': {'B', 'D'},
                'B': {'C', 'E'},
                'C': {'A', 'D'},
                'D': {'E', 'K'},
-               'E': {'C', 'H'},
-               'F': {'E', 'H', 'J'},
-               'G': {'F', 'J'},
-               'H': {'G'},
+               'E': {'C', 'h'},
+               'F': {'E', 'h', 'J'},
+               'g': {'F', 'J'},
+               'h': {'g'},
                'J': {'E'},
                'K': {'J'},
                'L': {'J', 'K'}},
@@ -217,38 +217,38 @@ def test2():
                1: {2, 3},
                2: {0},
                3: {}}]
-    for G in Graphs:
-        print(f'N strongly comps: {n_strongly_comp(G)}')
+    for g in graphs:
+        print(f'n strongly comps: {n_strongly_comp(g)}')
         print('')
 
 # Топологическая сортировка. Алгоритм Тарьяна.
 # Если орграф не содержит циклов, то его вершины
 # можно пронумеровать так, что любое ребро идет
 # от вершины с меньшим номером к вершине с большим.
-# Сложность по времени алгоритма Тарьяна - О(N),
-# где N - число вершин. Вершина - это число от 1
-# до N.
+# Сложность по времени алгоритма Тарьяна - О(n),
+# где n - число вершин. Вершина - это число от 1
+# до n.
 
 # вариант с множеством used
 
 
-def dfs_t(vertex, G, used, ans):
+def dfs_t(vertex, g, used, ans):
     used.add(vertex)
-    for neighbor in G[vertex]:
+    for neighbor in g[vertex]:
         if neighbor not in used:
-            dfs_t(neighbor, G, used, ans)
+            dfs_t(neighbor, g, used, ans)
     # добавляем вершину в список на обратном
     # ходу рекурсии
     ans.append(vertex)
 
 
-def topsort(G):
+def topsort(g):
     used = set()
     # список, в который будет сохраняться топологический порядок вершины
     ans = []
-    for vertex in G:
+    for vertex in g:
         if vertex not in used:
-            dfs_t(vertex, G, used, ans)
+            dfs_t(vertex, g, used, ans)
     # необходимо инвертировать список
     # так как элементы в него заносились
     # на обратном ходу рекурсии в dfs_t
@@ -258,7 +258,7 @@ def topsort(G):
 
 
 def test3():
-    Graphs = [{1: {2},
+    graphs = [{1: {2},
                2: {3},
                3: {1}},
               {1: {3, 4, 5},
@@ -285,32 +285,32 @@ def test3():
                4: {0, 1},
                5: {0, 2}}]
 
-    for i, G in enumerate(Graphs):
-        print(f'graph #{i} has topological order: {topsort(G)}')
+    for i, g in enumerate(graphs):
+        print(f'graph #{i} has topological order: {topsort(g)}')
 
 # вариант со списком visited
 
 
-def dfs_t_v(vertex, G, visited, ans):
+def dfs_t_v(vertex, g, visited, ans):
     visited[vertex] = True
-    for neighbor in G[vertex]:
+    for neighbor in g[vertex]:
         if not visited[neighbor]:
-            dfs_t_v(neighbor, G, visited, ans)
+            dfs_t_v(neighbor, g, visited, ans)
     # добавляем вершину в список на обратном
     # ходу рекурсии
     ans.append(vertex)
 
 
-def topsort_v(G):
+def topsort_v(g):
     # число вершин графа
-    n = len(G)
+    n = len(g)
     # вершина с индексом 0 фиктивная - ее нет в графе
     visited = [False] * (n+1)
     # список, в который будет сохраняться топологический порядок вершины
     ans = []
     for vertex in range(1, n+1):
         if not visited[vertex]:
-            dfs_t_v(vertex, G, visited, ans)
+            dfs_t_v(vertex, g, visited, ans)
     # необходимо инвертировать список
     # так как элементы в него заносились
     # на обратном ходу рекурсии в dfs_t
@@ -320,7 +320,7 @@ def topsort_v(G):
 
 
 def test4():
-    Graphs = [{1: {2},
+    graphs = [{1: {2},
                2: {3},
                3: {1}},
               {1: {3, 4, 5},
@@ -347,8 +347,8 @@ def test4():
                5: {1, 2},
                6: {1, 3}}]
 
-    for i, G in enumerate(Graphs):
-        print(f'graph #{i} has topological order: {topsort_v(G)}')
+    for i, g in enumerate(graphs):
+        print(f'graph #{i} has topological order: {topsort_v(g)}')
 
 
 # поиск цикла в орграфе
@@ -358,11 +358,11 @@ def test4():
 # 2 - вершина посещена на обратном ходе рекурсии dfs
 # если в ходе рекурсии встретилась смежная вершина цвета 1
 # значит найден цикл
-def dfs_cycle(vertex, G, colored):
+def dfs_cycle(vertex, g, colored):
     colored[vertex] = 1
-    for neighbor in G[vertex]:
+    for neighbor in g[vertex]:
         if colored[neighbor] == 0:
-            if dfs_cycle(neighbor, G, colored):
+            if dfs_cycle(neighbor, g, colored):
                 return True
         elif colored[neighbor] == 1:
             return True
@@ -370,16 +370,16 @@ def dfs_cycle(vertex, G, colored):
     return False
 
 
-def find_cycle(G):
-    colored = {v: 0 for v in G}
-    for vertex in G:
-        if dfs_cycle(vertex, G, colored):
+def find_cycle(g):
+    colored = {v: 0 for v in g}
+    for vertex in g:
+        if dfs_cycle(vertex, g, colored):
             return True
     return False
 
 
 def test5():
-    Graphs = [{1: {2},
+    graphs = [{1: {2},
                2: {3},
                3: {1}},
               {1: {3, 4, 5},
@@ -406,8 +406,8 @@ def test5():
                5: {1, 2},
                6: {1, 3}}]
 
-    for i, G in enumerate(Graphs):
-        print(f'graph #{i} has cycle: {find_cycle(G)}')
+    for i, g in enumerate(graphs):
+        print(f'graph #{i} has cycle: {find_cycle(g)}')
 
 
 if __name__ == '__main__':
