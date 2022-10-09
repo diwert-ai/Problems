db47ba0f16ecb619ace97d79787c085bec8d6fd6
diwert-ai <diwert08@gmail.com>
2022-10-09 17:03:55 +0300

    count chains- checkio

--- Problems/Checkio/Simple/count chains.py
+++ Problems/Checkio/Simple/count chains.py
@@ -1,4 +1,13 @@
-﻿from typing import List, Tuple
+# https://py.checkio.org/en/mission/count-chains/
+# In this mission you must count chains.
+#
+# You are given a list of details of the circle (tuple of X-coordinate, Y-coordinate, radius).
+# You have to return the number of groups of circles where their circumferences intersect.
+#
+# NOTE:
+#
+# Only one circle counts as one group.
+from typing import List, Tuple
 from math import sqrt
 from collections import deque
 
@@ -42,7 +51,18 @@ def test0():
 
 
 def test1():
-    pass
+    print("Example:")
+    print(count_chains([(1, 1, 1), (4, 2, 1), (4, 3, 1)]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert count_chains([(1, 1, 1), (4, 2, 1), (4, 3, 1)]) == 2, 'basic'
+    assert count_chains([(1, 1, 1), (2, 2, 1), (3, 3, 1)]) == 1, 'basic #2'
+    assert count_chains([(2, 2, 2), (4, 2, 2), (3, 4, 2)]) == 1, 'trinity'
+    assert count_chains([(2, 2, 1), (2, 2, 2)]) == 2, 'inclusion'
+    assert count_chains([(1, 1, 1), (1, 3, 1), (3, 1, 1), (3, 3, 1)]) == 4, 'adjacent'
+    assert count_chains([(0, 0, 1), (-1, 1, 1), (1, -1, 1), (-2, -2, 1)]) == 2, 'negative coordinates'
+    assert count_chains([(1, 3, 1), (2, 2, 1), (4, 2, 1), (5, 3, 1), (3, 3, 1)]) == 1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
 
 
 if __name__ == '__main__':
