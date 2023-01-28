718092029c029869c4618b719efe665a76eb88e4
diwert-ai <diwert08@gmail.com>
2023-01-28 10:53:51 +0300

    PEP8

--- Problems/Hiryanov/check sorted.py
+++ Problems/Hiryanov/check sorted.py
@@ -1,28 +1,30 @@
-﻿#проверка отсортированности массива за О(N) (однопроходный)
-#из лекции Хирьянова https://www.youtube.com/watch?v=2XFaK3bgT7w&list=PLRDzFCPr95fK7tr47883DFUbm4GeOjjc0&index=8
+﻿# проверка отсортированности массива за О(N) (однопроходный)
+# из лекции Хирьянова https://www.youtube.com/watch?v=2XFaK3bgT7w&list=PLRDzFCPr95fK7tr47883DFUbm4GeOjjc0&index=8
 
 from itertools import product
 
-def check_sorted(array:list,ascending=True):
-    sign = 2*int(ascending)-1
-    for i in range(len(array)-1):
-        if sign*array[i]>sign*array[i+1]:
+
+def check_sorted(array: list, ascending=True):
+    sign = 2 * int(ascending) - 1
+    for i in range(len(array) - 1):
+        if sign * array[i] > sign * array[i + 1]:
             return False
 
     return True
 
 
 def test0():
-    tests = [[0,1,2,3,4,5],
-             [0,2,1,3,4,4],
-             [2,2,2,2,2,2],
-             [6,4,3,2,1,0],
-             [4,4,4,4,4,1]]
+    tests = [[0, 1, 2, 3, 4, 5],
+             [0, 2, 1, 3, 4, 4],
+             [2, 2, 2, 2, 2, 2],
+             [6, 4, 3, 2, 1, 0],
+             [4, 4, 4, 4, 4, 1]]
 
-    asc = [True,False]
+    asc = [True, False]
 
-    for test,asc in product(tests,asc):
-        print(check_sorted(test,ascending=asc), sorted(test, reverse=not asc)==test)
+    for test, asc in product(tests, asc):
+        print(check_sorted(test, ascending=asc), sorted(test, reverse=not asc) == test)
+
 
 if __name__ == '__main__':
     test0()
