23ca41c6adb49850bd5eefaabda601154350d285
diwert-ai <diwert08@gmail.com>
2023-06-04 18:02:21 +0300

    PEP8

--- Problems/Hiryanov/floyd warshall paths.py
+++ Problems/Hiryanov/floyd warshall paths.py
@@ -1,13 +1,8 @@
-﻿# Алгоритм Флойда-Уоршелла. WFI
-# Из лекции Хирьянова:
-# https://www.youtube.com/watch?v=2N6YbTc-USw&list=PLRDzFCPr95fK7tr47883DFUbm4GeOjjc0&index=25
+﻿# Алгоритм Флойда-Уоршелла. WFI.
+# Из лекции Хирьянова: https://www.youtube.com/watch?v=2N6YbTc-USw&list=PLRDzFCPr95fK7tr47883DFUbm4GeOjjc0&index=25
 
-# Нахождение кратчайших путей сразу для всех вершин
-# (от каждой к каждой) взвешенного графа.
-# Требует О(N^3) времени.
-# Тип: динамическое программирование.
-# работает с отрицательными весами, но
-# не циклами отрицательного веса.
+# Нахождение кратчайших путей сразу для всех вершин (от каждой к каждой) взвешенного графа. Требует О(N^3) времени.
+# Тип: динамическое программирование. Работает с отрицательными весами, но не циклами отрицательного веса.
 
 from collections import deque
 
@@ -27,48 +22,44 @@ Graphs = [{1: {2: 2, 9: 15},
            'e': {'c': 12, 'f': 5},
            'f': {'c': 7, 'e': 5, 'd': 5}}]
 
-# пусть F_{i,j}^{0} - кратчайшее расстояние от вершины i к
-# вершине j в графе G при условии, что нельзя пользоваться
-# другим промежуточными вершинами. F_{i,j}^{1} - можно
-# пользоваться 1й промежуточной вершиной . F_{i,j}^{k} -
-# можно пользоваться первыми k штук вершин.
-# Тогда F_{i,j}^{k} = min (F_{i,j}^{k-1}, F_{i,k}^{k-1} + F_{k,j}^{k-1})
-# Ответ будет находиться в матрице F_{i,j}^{N} где N число вершин в G.
+# Пусть F_{i,j}^{0} - кратчайшее расстояние от вершины i к вершине j в графе G при условии, что нельзя пользоваться
+# другим промежуточными вершинами. F_{i,j}^{1} - можно пользоваться 1-й промежуточной вершиной. F_{i,j}^{k} - можно
+# пользоваться первыми k вершинами. Тогда F_{i,j}^{k} = min (F_{i,j}^{k-1}, F_{i,k}^{k-1} + F_{k,j}^{k-1}). Ответ будет
+# находиться в матрице F_{i,j}^{N} где N число вершин в G.
 
 
-# g_order - упорядоченный список вершин G
-# (для того чтобы алгоритм работал в т.ч., когда вершины
-# имееют буквенные названия)
-# в начале F[i][i] считаем 0, остальные расстояни +inf
-def wfi(G, g_order):
-    N = len(G)
-    F = [[0 if i == j else float('inf') for i in range(N)] for j in range(N)]
+# `g_order` - упорядоченный список вершин G (для того чтобы алгоритм работал в т.ч., когда вершины имеют буквенные
+# названия). В начале F[i][i] считаем 0, остальные расстояния +inf.
+def wfi(graph, g_order):
+    n = len(graph)
+    f = [[0 if i == j else float('inf') for i in range(n)] for j in range(n)]
     for i, v in enumerate(g_order):
         for j, u in enumerate(g_order):
-            if u in G[v]:
-                F[i][j] = G[v][u]
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                F[i][j] = min(F[i][j], F[i][k]+F[k][j])
-    return F
+            if u in graph[v]:
+                graph[i][j] = graph[v][u]
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                f[i][j] = min(f[i][j], f[i][k]+f[k][j])
+    return f
 
 
-def find_shortest_path(start, end, G):
+def find_shortest_path(start, end, graph):
     g_order = []
-    for v in G:
+    for v in graph:
         g_order.append(v)
-    F = wfi(G, g_order)
+    f = wfi(graph, g_order)
     i_start = g_order.index(start)
-    distances = {g: F[i_start][g_order.index(g)] for g in g_order}
+    distances = {g: f[i_start][g_order.index(g)] for g in g_order}
     if distances[end] == float('inf'):
         return None, distances
     path = [end]
     queue = deque([end])
     while queue:
         v = queue.popleft()
-        for u in G[v]:
-            if distances[u] + G[v][u] == distances[v]:
+        for u in graph[v]:
+            if distances[u] + graph[v][u] == distances[v]:
                 path.append(u)
                 queue.append(u)
                 break
@@ -77,8 +68,8 @@ def find_shortest_path(start, end, G):
 
 def test0():
     bounds = ((1, 8), ('a', 'e'))
-    for (start, end), G in zip(bounds, Graphs):
-        path, dists = find_shortest_path(start, end, G)
+    for (start, end), graph in zip(bounds, Graphs):
+        path, dists = find_shortest_path(start, end, graph)
         print(f'shortest path from {start} to {end}: {path}')
         print(f'distance from {start} to {end}: {dists[end]}')
         print(f'distances from {start} to vertexes: {dists}')
